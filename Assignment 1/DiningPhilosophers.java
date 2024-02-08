c38e87b7e052de65431e98177381cb3c4103914c
T0rt13 <mg6737lv@go.minnstate.edu>
2024-02-08 17:06:16 -0600

    Assignment completion
    Assignment completion

--- Assignment 1/DiningPhilosophers.java
+++ Assignment 1/DiningPhilosophers.java
@@ -1,16 +1,27 @@
 public class DiningPhilosophers {
-
+  
   public static void main(String[] args) throws InterruptedException {
-    Philosopher[] philosophers = new Philosopher[5];
-    Chopstick[] chopsticks = new Chopstick[5];
-    
-    for (int i = 0; i < 5; ++i)
+    int np = Integer.parseInt(args[0]); // number of philosophers (and chopsticks)
+    int nc = Integer.parseInt(args[1]); // number of cycles each philosopher performs
+    int tt = Integer.parseInt(args[2]); // max thinking time (ms)
+    int et = Integer.parseInt(args[3]); // max eating time (ms)
+    int rl = Integer.parseInt(args[4]); // If 0, all philosophers are right-handed. If 1, even-numbered philosophers are right-handed and odd-numbered philosophers are left-handed.
+    Philosopher[] philosophers = new Philosopher[np];
+    Chopstick[] chopsticks = new Chopstick[np];
+
+    for (int i = 0; i < np; ++i)
       chopsticks[i] = new Chopstick(i);
-    for (int i = 0; i < 5; ++i) {
-      philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % 5]);
+    for (int i = 0; i < np; ++i) {
+      // If rl = 0, all philosophers are right-handed. If rl = 1, only even-numbered philosophers are right handed.
+      if (rl == 0 || i % 2 == 0){
+        philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % np], i, nc, tt, et, 0);
+      }
+      else{
+        philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % np], i, nc, tt, et, 1);
+      }
       philosophers[i].start();
     }
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < np; ++i)
       philosophers[i].join();
   }
 }
--- Assignment 1/Philosopher.java
+++ Assignment 1/Philosopher.java
@@ -4,23 +4,76 @@ class Philosopher extends Thread {
   private Chopstick left, right;
   private Random random;
   private int thinkCount;
+  private int philosopherNumber;
+  private int numCycles;
+  private int maxThinkTime;
+  private int maxEatTime;
+  private int rightHanded;
 
-  public Philosopher(Chopstick left, Chopstick right) {
+  public Philosopher(Chopstick left, Chopstick right, int id, int nc, int tt, int et, int rl) {
     this.left = left; this.right = right;
     random = new Random();
+    this.philosopherNumber = id;
+    this.numCycles = nc;
+    if (tt == 0){ // if tt is  equal to 0, random.nextInt(maxThinkTime) would have an error.
+      this.maxThinkTime = 1000;
+    }
+    else{
+      this.maxThinkTime = tt;
+    }
+
+    if (et == 0){ // if et is equal to 0, random.nextInt(maxEatTime) would have an error.
+      this.maxEatTime = 1000;
+    }
+    else{
+      this.maxEatTime = et;
+    }
+    this.rightHanded = rl;
   }
 
   public void run() {
     try {
-      while(true) {
+      while(numCycles == 0 || thinkCount < numCycles) {
         ++thinkCount;
         if (thinkCount % 10 == 0)
-          System.out.println("Philosopher " + this + " has thought " + thinkCount + " times");
-        Thread.sleep(random.nextInt(1000));     // Think for a while
-        synchronized(left) {                    // Grab left chopstick 
-          synchronized(right) {                 // Grab right chopstick 
-            Thread.sleep(random.nextInt(1000)); // Eat for a while
+          System.out.println("Philosopher " + philosopherNumber + " has thought " + thinkCount + " times.");
+        int tempThink = random.nextInt(maxThinkTime);
+        // Think for a while
+        Thread.sleep(tempThink);
+        System.out.println("Philosopher " + philosopherNumber + " thinks for " + tempThink + " milliseconds.");
+         // grab dominant hand's chopstick first (right in this case)
+        if (rightHanded == 0){        
+          System.out.println("Philosopher " + philosopherNumber + " wants right chopstick.");
+          synchronized(right) {        
+            System.out.println("Philosopher " + philosopherNumber + " has right chopstick.");
+            System.out.println("Philosopher " + philosopherNumber + " wants left chopstick.");            
+            synchronized(left) {          
+              System.out.println("Philosopher " + philosopherNumber + " has left chopstick.");
+              int tempEat = random.nextInt(maxEatTime);
+              // Eat for a while after grabbing chopsticks
+              Thread.sleep(tempEat);
+              System.out.println("Philosopher " + philosopherNumber + " eats for " + tempEat + " milliseconds");
+            }
+            System.out.println("Philosopher " + philosopherNumber + " releases left chopstick.");
           }
+          System.out.println("Philosopher " + philosopherNumber + " releases right chopstick.");
+        }
+        // grab dominant hand's chopstick first (left in this case)
+        else{ 
+          System.out.println("Philosopher " + philosopherNumber + " wants left chopstick.");
+          synchronized(left) {        
+            System.out.println("Philosopher " + philosopherNumber + " has left chopstick.");
+            System.out.println("Philosopher " + philosopherNumber + " wants right chopstick.");            
+            synchronized(right) {          
+              System.out.println("Philosopher " + philosopherNumber + " has right chopstick.");
+              int tempEat = random.nextInt(maxEatTime);
+              // Eat for a while after grabbing chopsticks
+              Thread.sleep(tempEat);
+              System.out.println("Philosopher " + philosopherNumber + " eats for " + tempEat + " milliseconds");
+            }
+            System.out.println("Philosopher " + philosopherNumber + " releases right chopstick.");
+          }
+          System.out.println("Philosopher " + philosopherNumber + " releases left chopstick.");
         }
       }
     } catch(InterruptedException e) {}
